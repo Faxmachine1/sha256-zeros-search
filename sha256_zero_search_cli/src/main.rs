40824442f3d6e2fbb1f56830053486a07ed04e13
Faxmachine1 <139303880+Faxmachine1@users.noreply.github.com>
2023-09-03 21:38:19 +0300

    Add files via upload

---
+++ sha256_zero_search_cli/src/main.rs
@@ -0,0 +1,31 @@
+use clap::Parser;
+
+mod utils;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short)]
+    n: usize,
+
+    #[arg(short)]
+    f: u8,
+}
+
+fn main() {
+    let cli_args = Args::parse(); // {n: 0, f: 0}
+    let desired = "0".repeat(cli_args.n); // 0000
+    
+    let mut i = 0;
+    let mut found = 0;
+
+    while found < cli_args.f {
+        let hash = utils::generate_sha256(i);
+        
+        if hash.ends_with(&desired) {
+            println!("{}, {:?}", i, &hash[7..]);
+            found += 1
+        }
+
+        i += 1;
+    }
+}
