3a5c0d461d3d297b5979a7277297bc5e183aacaf
Faxmachine1 <139303880+Faxmachine1@users.noreply.github.com>
2023-09-30 17:56:56 +0300

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,7 @@ pub fn generate_sha256(number: u32) -> String {
     let number_bytes = number.to_string();
     let mut context = digest::Context::new(&digest::SHA256);
     
-    context.update(&number_bytes.as_bytes());
+    context.update(number_bytes.as_bytes());
 
     hex::encode(context.finish())
 }
